95fbdf817be9dba0131d8a2f9b7607177693cd5e
LittleBuddha-k <2998986128@qq.com>
2021-03-26 15:58:33 +0800

    修改shiro认证缓存器为redis 01

--- src/main/java/com/littlebuddha/recruit/common/config/shiro/ShiroConfig.java
+++ src/main/java/com/littlebuddha/recruit/common/config/shiro/ShiroConfig.java
@@ -3,6 +3,7 @@ package com.littlebuddha.recruit.common.config.shiro;
 import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
 import com.littlebuddha.recruit.common.config.shiro.realms.CustomerRealm;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
@@ -61,6 +62,14 @@ public class ShiroConfig {
         //设置散列次数
         hashedCredentialsMatcher.setHashIterations(1024);
         customerRealm.setCredentialsMatcher(hashedCredentialsMatcher);
+
+        //开启数据缓存
+        customerRealm.setCacheManager(new EhCacheManager());
+        customerRealm.setCachingEnabled(true);
+        customerRealm.setAuthenticationCachingEnabled(true);//开启认证缓存
+        customerRealm.setAuthenticationCacheName("authenticationCache");
+        customerRealm.setAuthorizationCachingEnabled(true);//开启授权缓存
+        customerRealm.setAuthorizationCacheName("authorizationCache");
         return customerRealm;
     }
 
---
+++ src/main/java/com/littlebuddha/recruit/common/config/shiro/cache/RedisCacheManager.java
@@ -0,0 +1,7 @@
+package com.littlebuddha.recruit.common.config.shiro.cache;
+
+/**
+ * 使用redis替换shiro的Ehcache缓存
+ */
+public class RedisCacheManager {
+}
